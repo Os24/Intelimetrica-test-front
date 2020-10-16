537867d4ac354e84b9b1fd803f3d12c365f236a5
Os24 <os.deleyja@gmail.com>
2020-10-15 21:55:25 -0500

    Updates

---
+++ precache-manifest.f802b7f0e4f2843d831448a2f2ea2584.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b8c44f94740b7dbf5a095356a56509c",
+    "url": "/Intelimetrica-test-front/index.html"
+  },
+  {
+    "revision": "f16502ed87c17d75a1db",
+    "url": "/Intelimetrica-test-front/static/css/main.a5de7016.chunk.css"
+  },
+  {
+    "revision": "0084f64d27fc10cc712f",
+    "url": "/Intelimetrica-test-front/static/js/2.0005fcac.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/Intelimetrica-test-front/static/js/2.0005fcac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f16502ed87c17d75a1db",
+    "url": "/Intelimetrica-test-front/static/js/main.c3939f1a.chunk.js"
+  },
+  {
+    "revision": "583b4aea8e20dc4ea6e1",
+    "url": "/Intelimetrica-test-front/static/js/runtime-main.eeb2fd89.js"
+  }
+]);
